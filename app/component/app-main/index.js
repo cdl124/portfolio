e4e60eee8a46dffc8812fb4ea1397555668b5881
Cortney Lane <cdlane124@gmail.com>
2016-09-24 22:10:52 -0700

    added app-homepage component

--- app/component/app-main/index.js
+++ app/component/app-main/index.js
@@ -9,6 +9,21 @@ angular.module('devPortfolio')
   return {
     restrict: 'E',
     template: require('./app-main.html'),
+    controller: 'AppMainController',
+    controllerAs: 'appMainCtrl',
+    bindToController: true,
     scope: {}
   };
-});
+})
+.controller('AppMainController', [AppMainController]);
+
+function AppMainController() {
+
+  this.selectedPage = 'home';
+  this.selectPage = function(page) {
+    if (page === 'home' || page === 'portfolio' || page === 'about') {
+      this.selectedPage = page;
+      return this.selectedPage;
+    }
+  };
+}
