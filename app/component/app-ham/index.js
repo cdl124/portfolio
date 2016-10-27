eafb9474b5b29fb43a58e009bfcbea97c9f301cb
Cortney Lane <cdlane124@gmail.com>
2016-10-26 21:32:46 -0700

    changed app-ham from .component to .directive; ham menu now hides upon menu option click

--- app/component/app-ham/index.js
+++ app/component/app-ham/index.js
@@ -5,17 +5,25 @@ require('./_app-ham.scss');
 const angular = require('angular');
 
 angular.module('devPortfolio')
-.component('appHam', {
-  template: require('./app-ham.html'),
-  controller: 'AppHamController',
-  controllerAs: 'appHamCtrl',
-  // scope: {
-  //   selectPage: '&'
-  // }
+.directive('appHam', function(){
+  return {
+    restrict: 'E',
+    template: require('./app-ham.html'),
+    controller: 'AppHamController',
+    controllerAs: 'appHamCtrl',
+    scope: {
+      selectPage: '&'
+    }
+  };
 })
 .controller('AppHamController', ['$log', AppHamController]);
 
 function AppHamController($log){
   $log.debug('HamController invoked');
   this.showNav = false;
+
+  this.closeNav = function(){
+    this.showNav = false;
+  };
+  
 }
